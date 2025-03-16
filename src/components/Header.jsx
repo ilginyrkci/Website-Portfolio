da55dd50942f9b5f998d33ebb23503811f1c27b4
unknown <yilginhabibe@gmail.com>
2025-03-17 01:02:32 +0300

    Header sayfası güncellendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,27 @@ export default function Header({ setActiveSection }) {
     >
       <div className="container mx-auto flex justify-between items-center p-4">
         <h1 className="text-3xl font-bold color3">Ilgın Habibe Yürekçi</h1>
-        <nav className="space-x-6 flex flex-wrap justify-center md:justify-end">
-          {[ 
+        {/* Mobile Menu Button for smaller screens */}
+        <div className="md:hidden flex items-center">
+          <button className="text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <nav className="space-x-6 flex flex-wrap justify-center md:justify-end w-full md:w-auto">
+          {[
             { section: "home", label: "Ana Sayfa" },
             { section: "about", label: "Hakkımda" },
             { section: "skills", label: "Yetenekler" },
